ac1cc15c4bea18ca606ae566e6f421a373f81396
seob717 <seob717@seob717ui-MacBookPro.local>
2021-02-05 16:15:32 +0900

    테이블 만들기

--- src/containers/Socket.js
+++ src/containers/Socket.js
@@ -5,13 +5,14 @@ const Socket = () => {
     const [connection, setConnection] = useState(false);
 
     const [protocol, setProtocol] = useState("sftp");
-    const [host, setHost] = useState("");
-    const [port, setPort] = useState("");
-    const [user, setUser] = useState("");
-    const [password, setPassword] = useState("");
+    const [host, setHost] = useState("211.253.10.9");
+    const [port, setPort] = useState("10021");
+    const [user, setUser] = useState("root");
+    const [password, setPassword] = useState("Netand141)");
 
     const [uuid, setUuid] = useState("")
     const [url, setUrl] = useState("")
+
 
     //원격서버 연결하기
     const connectServer = () => {
@@ -25,16 +26,10 @@ const Socket = () => {
         }
         const serverUrl = `ws://${host}:8080/ws/${protocol}`
         const ws = new WebSocket(serverUrl)
-
-        if (ws.readyState !== 1) {
-            console.log("연결중...")
-            // 잘못된 프로토콜 및 호스트 넘버 입력시 에러 핸들링!
-        }
-
         ws.onopen = (evt) => {
-            ws.onerror = (evt) => {
-                console.log(evt.data)
-            }
+
+            console.log('연결되었습니다.')
+
             ws.send(JSON.stringify({
                 "requestType": "Connect",
                 "host": host,
@@ -47,18 +42,30 @@ const Socket = () => {
                 console.log(JSON.parse(evt.data))
                 setUuid(JSON.parse(evt.data).uuid)
                 setConnection(true)
-            }
-            ws.onclose = (evt) => {
-                if (evt.code !== 1000) {
-                    console.log(`에러코드 ${evt.code} 로 서버 연결이 종료됩니다.`)
+                ws.send(JSON.stringify({
+                    "requestType" : "Command",
+                    "uuid" : JSON.parse(evt.data).uuid,
+                    "message" : "ls /etc"
+                }))
+                ws.onmessage = (evt)=>{
+                    console.log(JSON.parse(evt.data))
                 }
-                console.log('서버 연결이 종료되었습니다.')
-                setConnection(false)
             }
+        }
+        ws.onclose = (evt) => {
+            if (evt.code !== 1000) {
+                console.log(`에러코드 ${evt.code} 로 서버 연결이 종료됩니다.`)
+            }
+            console.log('서버 연결이 종료되었습니다.')
+            setConnection(false)
+        }
+        ws.onerror = (evt) => {
+            console.log(evt.data)
         }
         setUrl(serverUrl)
     }
 
+    // 원격 서버 연결 해제하기
     const disconnectServer = () => {
         if (connection === false) {
             alert("연결된 서버가 없습니다.")
@@ -77,19 +84,23 @@ const Socket = () => {
                 console.log('연결이 해제되었습니다.')
                 console.log(JSON.parse(evt.data))
             }
-            ws.onclose = (evt) => {
-                console.log(`연결해제 에러코드 : ${evt.code}`)
-                setConnection(false)
-            }
-            ws.onerror = (evt) => {
-                console.log(`onerror excution`)
-                setConnection(false)
-            }
             setUrl("")
             setUuid("")
             setConnection(false)
         }
+        ws.onerror = (evt) => {
+            console.log(`onerror excution`)
+            setConnection(false)
+        }
+        ws.onclose = (evt) => {
+            if (evt.code !== 1000) {
+                console.log(`에러코드 ${evt.code} 로 서버 연결이 종료됩니다.`)
+            }
+            console.log('서버 연결이 종료되었습니다.')
+            setConnection(false)
+        }
     }
+
     return (
         <div style={{padding: "24px"}}>
             {/* 웹 소켓 서버 연결 */}
@@ -102,12 +113,17 @@ const Socket = () => {
             <input type="text" name="port" value={port} onChange={(e) => setPort(e.target.value)}
                    placeholder="원격 서버포트"/>
             <input type="text" name="user" value={user} onChange={(e) => setUser(e.target.value)} placeholder="사용자"/>
-            <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}
+            <input type="text" name="password" value={password} onChange={(e) => setPassword(e.target.value)}
                    placeholder="비밀번호"/>
             <button onClick={connectServer}>서버 연결</button>
             <button onClick={disconnectServer}>서버 연결해제</button>
             {/*  */}
-            {/*  */}
+            {/* 명령어 입력하기 */}
+            {connection &&
+            <div>
+                <h3>디렉토리 테이블</h3>
+            </div>
+            }
             {/*  */}
             {/*<input type="file"/>*/}
 
