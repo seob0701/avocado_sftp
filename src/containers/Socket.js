749c9cceffe8906696f347fc0af604b3bb24e5b1
seob717 <seob717@seob717ui-MacBookPro.local>
2021-02-05 11:36:11 +0900

    호스트, 포트넘버, 프로토콜 예외처리 중

--- src/containers/Socket.js
+++ src/containers/Socket.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 
 
 const Socket = () => {
+    const [connection, setConnection] = useState(false);
+
     const [protocol, setProtocol] = useState("sftp");
     const [host, setHost] = useState("");
     const [port, setPort] = useState("");
@@ -17,15 +19,22 @@ const Socket = () => {
             alert("입력하지 않은 값이 있습니다.")
             return
         }
-        if(uuid !== "" || url !==""){
+        if (connection === true) {
             alert("이미 서버가 연결되어있습니다.")
             return
         }
         const serverUrl = `ws://${host}:8080/ws/${protocol}`
         const ws = new WebSocket(serverUrl)
 
-        ws.onopen = (evt) => {
+        if (ws.readyState !== 1) {
             console.log("연결중...")
+            // 잘못된 프로토콜 및 호스트 넘버 입력시 에러 핸들링!
+        }
+
+        ws.onopen = (evt) => {
+            ws.onerror = (evt) => {
+                console.log(evt.data)
+            }
             ws.send(JSON.stringify({
                 "requestType": "Connect",
                 "host": host,
@@ -34,38 +43,51 @@ const Socket = () => {
                 "port": port
             }))
             ws.onmessage = (evt) => {
+                console.log('원격 서버와 연결되었습니다.')
                 console.log(JSON.parse(evt.data))
                 setUuid(JSON.parse(evt.data).uuid)
-                ws.onclose = () => {}
+                setConnection(true)
             }
-            ws.onError = (evt)=>{
-                console.log(evt.data)
+            ws.onclose = (evt) => {
+                if (evt.code !== 1000) {
+                    console.log(`에러코드 ${evt.code} 로 서버 연결이 종료됩니다.`)
+                }
+                console.log('서버 연결이 종료되었습니다.')
+                setConnection(false)
             }
         }
         setUrl(serverUrl)
     }
 
     const disconnectServer = () => {
-        if (url === "" || uuid === "") {
+        if (connection === false) {
             alert("연결된 서버가 없습니다.")
             return
         }
         const ws = new WebSocket(url)
-        console.log('연결 해제 중...')
+        if (ws.readyState !== 3) {
+            console.log('연결 해제 중...')
+        }
         ws.onopen = (evt) => {
             ws.send(JSON.stringify({
                 "requestType": "Disconnect",
                 "uuid": uuid
             }))
             ws.onmessage = (evt) => {
+                console.log('연결이 해제되었습니다.')
                 console.log(JSON.parse(evt.data))
-                ws.onclose = () => {}
             }
-            ws.onError = (evt)=>{
-                console.log(evt.data)
+            ws.onclose = (evt) => {
+                console.log(`연결해제 에러코드 : ${evt.code}`)
+                setConnection(false)
+            }
+            ws.onerror = (evt) => {
+                console.log(`onerror excution`)
+                setConnection(false)
             }
             setUrl("")
             setUuid("")
+            setConnection(false)
         }
     }
     return (
